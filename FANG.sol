47a18141ad18afb2de1d649f4258d0c8fed3526e
WolfpackBOT <42703853+WolfpackBOT@users.noreply.github.com>
2019-08-28 16:52:10 -0400

    Update FANG.sol

--- FANG.sol
+++ FANG.sol
@@ -68,7 +68,7 @@ contract Ownable {
   * @dev Allows the current owner to transfer control of the contract to a newOwner.
   * @param newOwner The address to transfer ownership to.
   */
-  function transferOwnership(address newOwner) public onlyOwner {
+  function transferOwnership(address payable newOwner) public onlyOwner {
     _transferOwnership(newOwner);
   }
 
@@ -76,7 +76,7 @@ contract Ownable {
   * @dev Transfers control of the contract to a newOwner.
   * @param newOwner The address to transfer ownership to.
   */
-  function _transferOwnership(address newOwner) internal {
+  function _transferOwnership(address payable newOwner) internal {
     require(newOwner != address(0));
     emit OwnershipTransferred(_owner, newOwner);
     _owner = newOwner;
